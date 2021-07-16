cb9f38b7cfe27d30915d648620b4b84b607b29e0
uttam-reddy <30924574+uttam-reddy@users.noreply.github.com>
2021-07-16 22:48:17 +0530

    bootstrapped application

---
+++ react-ap/src/actions/store.js
@@ -0,0 +1,14 @@
+import { createStore,applyMiddleware,compose } from "redux";
+import thunk from "redux-thunk";
+
+
+export const store =createStore(
+    {},
+    compose(
+        applyMiddleware(thunk)
+    )
+
+)
+
+
+
